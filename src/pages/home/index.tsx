4a890a326067b13bebeaa0231c26a6f2a1f15a45
{Samshh} <{samdacs2@gmail.com}>
2024-10-26 12:41:05 +0800

    mapua mcm

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <section>
-      <div className="w-full flex justify-start items-center h-full">
+      <div className="w-full flex flex-col justify-center items-start h-full">
+        <h3 className="font-normal"><em>Mapúa MCM</em></h3>
         <h1><span className="text-red">College of Computer</span> <br/> & <span className="text-blue">Information Science</span></h1>
       </div>
     </section>
