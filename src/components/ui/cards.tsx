ebafcd83ecac948c3fa90e4543ef7f534848e1f3
Samshh <samdacs2@gmail.com>
2024-10-28 04:34:23 +0800

    removed rounded

--- src/components/ui/cards.tsx
+++ src/components/ui/cards.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 
 export default function Card({image, title, children, onClick}: CardProps) {
   return (
-    <div onClick={onClick} className="flex cursor-pointer flex-col gap-[1rem] justify-start items-center text-center p-[2rem] rounded-lg border-[1px] border-border bg-[#fff] shadow-md w-full h-full transition-all ease-linear hover:scale-105">
+    <div onClick={onClick} className="flex cursor-pointer flex-col gap-[1rem] justify-start items-center text-center p-[2rem] border-[1px] border-border bg-[#fff] shadow-md w-full h-full transition-all ease-linear hover:scale-105">
       {
         image && (
           <img src={image} alt={title} className="w-full h-auto" />
