09916296223f84cef2d0b98d2b5715b2d6e3137f
Samshh <samdacs2@gmail.com>
2024-10-29 09:23:12 +0800

    fixed padding issue

--- src/pages/aboutus/components/MeetTheTeam.tsx
+++ src/pages/aboutus/components/MeetTheTeam.tsx
@@ -2,20 +2,20 @@ import teamData from "./team.json";
 
 export default function MeetTheTeam() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[3rem] md:gap-[2rem] lg:gap-[4rem] w-full">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[3rem] w-full">
       {teamData.team.map((member) => (
         <div
           key={member.email}
-          className="flex flex-col justify-center items-center text-center gap-[1rem]"
+          className="flex flex-col h-full justify-start items-center text-center gap-[1rem]"
         >
           <img
             className="h-[260px] w-auto rounded-full shadow-lg"
             src={member.image}
             alt="faculty"
           />
-          <div>
+          <div className="flex flex-col items-center">
             <h4 className="text-regular">{member.name}</h4>
-            {member.title.split(',').map((title, index) => (
+            {member.title.split(",").map((title, index) => (
               <p key={index} className="text-regular">
                 {title.trim()}
               </p>
